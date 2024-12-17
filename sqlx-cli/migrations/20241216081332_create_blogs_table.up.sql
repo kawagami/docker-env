d01dc54674c14b78ece3c6ff8949dba5a16ac708
kawagami <joelai1988@gmail.com>
2024-12-17 20:52:26 +0800

    update: 更新 blogs 表格內容

--- sqlx-cli/migrations/20241216081332_create_blogs_table.up.sql
+++ sqlx-cli/migrations/20241216081332_create_blogs_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE blogs (
     id UUID PRIMARY KEY,
     markdown TEXT NOT NULL,
     html TEXT NOT NULL,
+    tocs TEXT[] DEFAULT '{}'::TEXT[], 
     tags TEXT[] DEFAULT '{}'::TEXT[], 
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
