5c22a7cccfd0e4ebb69864a0df379057cd2b34c6
kawagami <joelai1988@gmail.com>
2026-06-05 01:06:44 +0800

    fix: issue-cert.sh 加入 --entrypoint certbot 覆蓋自訂 entrypoint

--- shells/issue-cert.sh
+++ shells/issue-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run from docker-env directory
-docker compose run --rm certbot certonly \
+docker compose run --rm --entrypoint certbot certbot certonly \
   --dns-cloudflare \
   --dns-cloudflare-credentials /etc/cloudflare/cloudflare.ini \
   --email joelai1988@gmail.com \
